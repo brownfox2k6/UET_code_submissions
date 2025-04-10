6b046376a7f793d534697f44cef80865bf65fd7e
Trọng Đại Nguyễn <brfox2k6@gmail.com>
2025-04-10 12:56:03 +0700

    Update Module 07.../Bài tập tại lớp/06.cpp

--- INT2215 - Lập trình nâng cao/Module 07 - Các thao tác nâng cao (ND7)/Bài tập tại lớp/06.cpp
+++ INT2215 - Lập trình nâng cao/Module 07 - Các thao tác nâng cao (ND7)/Bài tập tại lớp/06.cpp
@@ -1,64 +1,40 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
+// cộng hai số đã được đảo ngược lại
+// ví dụ a=465, b=298 thì res=6541, vì 564+892=1456
 string add(const string &a, const string &b) {
-  string x(rbegin(a), rend(a));
-  string y(rbegin(b), rend(b));
-  int n = x.length();
-  int m = y.length();
-  if (n < m) {
-    swap(x, y);
-    swap(n, m);
-  }
-  int carry = 0;
   string res;
-  for (int i = 0; i < n; ++i) {
-    int cur = carry + x[i] - '0';
-    if (i < m) {
-      cur += y[i] - '0';
-    }
+  bool carry = 0;
+  for (size_t i = 0; i < max(a.length(), b.length()); ++i) {
+    int cur = carry;
+    if (i < a.length()) cur += a[i] - '0';
+    if (i < b.length()) cur += b[i] - '0';
     res.push_back(cur % 10 + '0');
     carry = cur / 10;
   }
-  if (carry) {
-    res.push_back('1');
-  }
-  reverse(begin(res), end(res));
+  if (carry) res.push_back('1');
   return res;
 }
 
 int main() {
-  string a, b, c;
-  cin >> a >> b >> c;
-
-  size_t i = a.find('?');
-  if (i != string::npos) {
-    for (char x : "0123456789") {
-      a[i] = x;
-      if (add(a, b) == c) {
-        cout << x;
-        return 0;
+  string a[3];
+  pair<size_t, size_t> unknown;
+  for (size_t i = 0; i < 3; ++i) {
+    cin >> a[i];
+    reverse(begin(a[i]), end(a[i])); // đảo ngược lại để thực hiện phép cộng dễ hơn
+    for (size_t j = 0; j < a[i].length(); ++j) {
+      if (a[i][j] == '?') {
+        unknown = {i, j};
       }
     }
   }
-  i = b.find('?');
-  if (i != string::npos) {
-    for (char x : "0123456789") {
-      b[i] = x;
-      if (add(a, b) == c) {
-        cout << x;
-        return 0;
-      }
-    }
-  }
-  i = c.find('?');
-  if (i != string::npos) {
-    for (char x : "0123456789") {
-      c[i] = x;
-      if (add(a, b) == c) {
-        cout << x;
-        return 0;
-      }
+  for (char c : "0123456789") {
+    a[unknown.first][unknown.second] = c;
+    if (add(a[0], a[1]) == a[2]) {
+      cout << c;
+      return 0;
     }
   }
 }
